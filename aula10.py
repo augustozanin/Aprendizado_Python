4359010504438c3dc54fbd02cf52b1184574964a
Augusto Zanin <augustozann@gmail.com>
2023-11-27 15:43:55 -0300

    Estruturas condicionais

---
+++ aula10.py
@@ -0,0 +1,80 @@
+# Desafios Testes condicionais
+import random
+'''
+num_random = random.randrange(0,6)
+num_user = int(input('chute um numero: '))
+
+if num_random == num_user:
+    print(f'Parabéns você acertou')
+else:
+    print(f'Você errou {num_random}, e você digitou {num_user}')
+
+speed = float(input('Insira a velocidade do carro: '))
+
+
+def calculator_fine(speed_value):
+    resul = speed_value - 80
+    fine = 7 * resul
+    return f'O valor da multa sera de {fine}'
+
+
+if speed > 80:
+    print('Excedeu a velocidade!')
+    print(calculator_fine(speed))
+else:
+    print('Without fine')
+
+# Ver se o numero é par ou impar
+
+num = float(input('numero: '))
+resul = num % 2
+if resul == 0:
+    print(f'numero {num} é par')
+else:
+    print(f'numero {num} é ímpar')
+
+
+km = float(input('Insira os kms: '))
+if km <= 200:
+    resul = 0.50 * km
+    print(f'Sua viagem custa {resul}')
+else:
+    resul = 0.45 * km
+    print(f'Sua viagem custa {resul}')
+
+
+year = int(input('Insira o ano: '))
+resul = year % 4
+if resul == 0:
+    print(f'O ano {year} é bissexto')
+else:
+    print(f'O ano {year} não é bissexto')
+
+num = input('Insira tres numeros: (?,?,?)')
+array = num.split(',')
+print(f'O maior numero é ', max(array))
+print(f'O menor numero é ', min(array))
+
+salary = float(input('Insira seu salario: '))
+if salary > 1250:
+    resul = salary * 0.10
+    resul_final = salary + resul
+    print(f'Com o aumento ficou {resul_final}')
+else:
+    resul = salary * 0.15
+    resul_final = salary + resul
+    print(f'Com o aumento ficou {resul_final}')
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
