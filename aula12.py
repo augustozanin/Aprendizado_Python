3fe8b2acbbb5bf6e0e3214b1a309f7578e482766
Augusto Zanin <augustozann@gmail.com>
2023-11-28 15:51:35 -0300

    Condições aninhadas

---
+++ aula12.py
@@ -0,0 +1,36 @@
+# Condições aninhadas
+import datetime
+"""
+valor_casa = float(input('Insira o valor da casa: '))
+salario_comprador = float(input('Insira o salário do comprador: '))
+anos = int(input('Insira em quantos anos vai pagar: '))
+qtd_meses = anos * 12
+per_salario = salario_comprador * 0.30
+valor_mensal = valor_casa / qtd_meses
+if valor_mensal > per_salario:
+    print(f'O valor foi execedido {per_salario} e o valor por mês é {valor_mensal}')
+    print(f'Empréstimo negado')
+else:
+    print(f'Empréstimo aprovado')
+
+n1 = int(input('Insira um numero: '))
+n2 = int(input('Insira um numero: '))
+if n1 > n2:
+    print(f'o primeiro valor é maior')
+elif n2 > n1:
+    print(f'O segundo valor é maior')
+elif n1 == n2:
+    print(f'Os valores são iguais')
+
+ano_nascimento = int(input('Insira o ano de nascimento: '))
+data_atual = datetime.date.today().year
+idade = data_atual - ano_nascimento
+if idade == 18:
+    print(f'Tem que se alistar! esse ano!')
+elif idade < 18:
+    print(f'Ainda terá que se alistar')
+elif idade > 18:
+    print(f'ja passou do tempo de alistamento')
+
+print(idade)
+"""
